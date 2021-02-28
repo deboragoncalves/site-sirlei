e477e8b1afd8dae10b67b2da9043ab5f1be36529
Débora Gonçalves <deboracristinapereira123@gmail.com>
2021-02-28 12:02:43 -0300

    Ajustes index

--- js/index-js.js
+++ js/index-js.js
@@ -1,40 +1,40 @@
 function toggleTherapies() {
 
-    var display = document.getElementsByClassName("terapiasLista").style.display;
+    var display = document.getElementById("terapiasLista").style.display;
 
     if (window.matchMedia("(min-width: 1200px)").matches) {
-        document.getElementsByClassName("linhaTerapias").style.maxWidth = "8%";
-        document.getElementsByClassName("terapiasLista").style.width = "250%";
+        document.getElementById("linhaTerapias").style.maxWidth = "8%";
+        document.getElementById("terapiasLista").style.width = "250%";
     } else {
-        document.getElementsByClassName("linhaTerapias").style.maxWidth = "100%";
+        document.getElementById("linhaTerapias").style.maxWidth = "100%";
     }
 
     if (display == "block") {
-        document.getElementsByClassName("terapiasLista").style.display = "none";
+        document.getElementById("terapiasLista").style.display = "none";
     } else {
-        document.getElementsByClassName("terapiasLista").style.display = "block";
+        document.getElementById("terapiasLista").style.display = "block";
     }
 
 }
 
 function toggleMenu() {
     if (window.matchMedia("(max-width: 1200px)").matches) {
-        document.getElementsByClassName("menu").style.paddingTop = "20px";
-        document.getElementsByClassName("menu").style.marginTop = "-150px";
-        document.getElementsByClassName("menu").style.float = "right";
+        document.getElementById("menu").style.paddingTop = "20px";
+        document.getElementById("menu").style.marginTop = "-150px";
+        document.getElementById("menu").style.float = "right";
 
         var displayMenu = document.getElementById("menu").style.display;
 
         if (displayMenu == "none") {
-            document.getElementsByClassName("menu").style.display = "block";
-            document.getElementsByClassName("labelMenu").style.marginTop = "-150px";
-            document.getElementsByClassName("whatsapp").style.display = "none";
-            document.getElementsByClassName("rodape").style.display = "none";
+            document.getElementById("menu").style.display = "block";
+            document.getElementById("labelMenu").style.marginTop = "-150px";
+            document.getElementById("whatsapp").style.display = "none";
+            document.getElementById("rodape").style.display = "none";
         } else {
-            document.getElementsByClassName("menu").style.display = "none";
-            document.getElementsByClassName("labelMenu").style.marginTop = "-120px";
-            document.getElementsByClassName("whatsapp").style.display = "block";
-            document.getElementsByClassName("rodape").style.display = "block";
+            document.getElementById("menu").style.display = "none";
+            document.getElementById("labelMenu").style.marginTop = "-120px";
+            document.getElementById("whatsapp").style.display = "block";
+            document.getElementById("rodape").style.display = "block";
         }
     }
 }
