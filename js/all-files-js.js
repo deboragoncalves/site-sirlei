5808006acf784b72f92e1992dae604a9870f9f21
deboracristinapereira123@gmail.com <deboracristinapereira123@gmail.com>
2021-03-28 00:51:46 -0300

    Formatacao

--- js/all-files-js.js
+++ js/all-files-js.js
@@ -1,46 +1,42 @@
 window.onload = function () {
-
-    document.getElementById("terapiasLista").style.display = "none";
-
-}
+  document.getElementById("terapiasLista").style.display = "none";
+};
 
 function toggleTherapies() {
+  var display = document.getElementById("terapiasLista").style.display;
 
-    var display = document.getElementById("terapiasLista").style.display;
+  if (window.matchMedia("(min-width: 1200px)").matches) {
+    document.getElementById("linhaTerapias").style.maxWidth = "8%";
+    document.getElementById("terapiasLista").style.width = "250%";
+  } else {
+    document.getElementById("linhaTerapias").style.maxWidth = "100%";
+  }
 
-    if (window.matchMedia("(min-width: 1200px)").matches) {
-        document.getElementById("linhaTerapias").style.maxWidth = "8%";
-        document.getElementById("terapiasLista").style.width = "250%";
-    } else {
-        document.getElementById("linhaTerapias").style.maxWidth = "100%";
-    }
-
-    if (display == "block") {
-        document.getElementById("terapiasLista").style.display = "none";
-    } else {
-        document.getElementById("terapiasLista").style.display = "block";
-    }
-
+  if (display == "block") {
+    document.getElementById("terapiasLista").style.display = "none";
+  } else {
+    document.getElementById("terapiasLista").style.display = "block";
+  }
 }
 
 function toggleMenu() {
-    if (window.matchMedia("(max-width: 1200px)").matches) {
-        document.getElementById("menu").style.paddingTop = "20px";
-        document.getElementById("menu").style.marginTop = "-150px";
-        document.getElementById("menu").style.float = "right";
+  if (window.matchMedia("(max-width: 1200px)").matches) {
+    document.getElementById("menu").style.paddingTop = "20px";
+    document.getElementById("menu").style.marginTop = "-150px";
+    document.getElementById("menu").style.float = "right";
 
-        var displayMenu = document.getElementById("menu").style.display;
+    var displayMenu = document.getElementById("menu").style.display;
 
-        if (displayMenu == "none") {
-            document.getElementById("menu").style.display = "block";
-            document.getElementById("labelMenu").style.marginTop = "-150px";
-            document.getElementById("whatsapp").style.display = "none";
-            document.getElementById("rodape").style.display = "none";
-        } else {
-            document.getElementById("menu").style.display = "none";
-            document.getElementById("labelMenu").style.marginTop = "-120px";
-            document.getElementById("whatsapp").style.display = "block";
-            document.getElementById("rodape").style.display = "block";
-        }
+    if (displayMenu == "none") {
+      document.getElementById("menu").style.display = "block";
+      document.getElementById("labelMenu").style.marginTop = "-150px";
+      document.getElementById("whatsapp").style.display = "none";
+      document.getElementById("rodape").style.display = "none";
+    } else {
+      document.getElementById("menu").style.display = "none";
+      document.getElementById("labelMenu").style.marginTop = "-120px";
+      document.getElementById("whatsapp").style.display = "block";
+      document.getElementById("rodape").style.display = "block";
     }
+  }
 }
--- js/therapy-yoni.js
+++ js/therapy-yoni.js
@@ -1,116 +1,112 @@
 window.onload = function () {
-
-    document.getElementById("terapiasLista").style.display = "none";
-
-}
+  document.getElementById("terapiasLista").style.display = "none";
+};
 
 function toggleTherapies() {
+  var display = document.getElementById("terapiasLista").style.display;
 
-    var display = document.getElementById("terapiasLista").style.display;
+  if (window.matchMedia("(min-width: 1200px)").matches) {
+    document.getElementById("linhaTerapias").style.maxWidth = "8%";
+    document.getElementById("terapiasLista").style.width = "250%";
+  } else {
+    document.getElementById("linhaTerapias").style.maxWidth = "100%";
+  }
 
-    if (window.matchMedia("(min-width: 1200px)").matches) {
-        document.getElementById("linhaTerapias").style.maxWidth = "8%";
-        document.getElementById("terapiasLista").style.width = "250%";
-    } else {
-        document.getElementById("linhaTerapias").style.maxWidth = "100%";
-    }
-
-    if (display == "block") {
-        document.getElementById("terapiasLista").style.display = "none";
-    } else {
-        document.getElementById("terapiasLista").style.display = "block";
-    }
-
+  if (display == "block") {
+    document.getElementById("terapiasLista").style.display = "none";
+  } else {
+    document.getElementById("terapiasLista").style.display = "block";
+  }
 }
 
 function toggleMenu() {
-    if (window.matchMedia("(max-width: 1200px)").matches) {
-        document.getElementById("menu").style.paddingTop = "20px";
-        document.getElementById("menu").style.marginTop = "-150px";
-        document.getElementById("menu").style.float = "right";
+  if (window.matchMedia("(max-width: 1200px)").matches) {
+    document.getElementById("menu").style.paddingTop = "20px";
+    document.getElementById("menu").style.marginTop = "-150px";
+    document.getElementById("menu").style.float = "right";
 
-        var displayMenu = document.getElementById("menu").style.display;
+    var displayMenu = document.getElementById("menu").style.display;
 
-        if (displayMenu == "none") {
-            document.getElementById("menu").style.display = "block";
-            document.getElementById("labelMenu").style.marginTop = "-150px";
-            document.getElementById("whatsapp").style.display = "none";
-            document.getElementById("rodape").style.display = "none";
-        } else {
-            document.getElementById("menu").style.display = "none";
-            document.getElementById("labelMenu").style.marginTop = "-120px";
-            document.getElementById("whatsapp").style.display = "block";
-            document.getElementById("rodape").style.display = "block";
-        }
+    if (displayMenu == "none") {
+      document.getElementById("menu").style.display = "block";
+      document.getElementById("labelMenu").style.marginTop = "-150px";
+      document.getElementById("whatsapp").style.display = "none";
+      document.getElementById("rodape").style.display = "none";
+    } else {
+      document.getElementById("menu").style.display = "none";
+      document.getElementById("labelMenu").style.marginTop = "-120px";
+      document.getElementById("whatsapp").style.display = "block";
+      document.getElementById("rodape").style.display = "block";
     }
+  }
 }
 
 function toggleAnswer1() {
-    var answer1Display = document.getElementById("resposta1").style.display;
+  var answer1Display = document.getElementById("resposta1").style.display;
 
-    if (answer1Display == "none") {
-        document.getElementById("resposta1").style.display = "block";
-    } else {
-        document.getElementById("resposta1").style.display = "none";
-    }
+  if (answer1Display == "none") {
+    document.getElementById("resposta1").style.display = "block";
+  } else {
+    document.getElementById("resposta1").style.display = "none";
+  }
 }
 
 function toggleAnswer2() {
-    var answer2Display = document.getElementById("resposta2").style.display;
+  var answer2Display = document.getElementById("resposta2").style.display;
 
-    if (answer2Display == "none") {
-        document.getElementById("resposta2").style.display = "block";
-    } else {
-        document.getElementById("resposta2").style.display = "none";
-    }
+  if (answer2Display == "none") {
+    document.getElementById("resposta2").style.display = "block";
+  } else {
+    document.getElementById("resposta2").style.display = "none";
+  }
 }
 
 function toggleAnswer3() {
-    var answer3Display = document.getElementById("resposta3").style.display;
+  var answer3Display = document.getElementById("resposta3").style.display;
 
-    if (answer3Display == "none") {
-        document.getElementById("resposta3").style.display = "block";
-    } else {
-        document.getElementById("resposta3").style.display = "none";
-    }
+  if (answer3Display == "none") {
+    document.getElementById("resposta3").style.display = "block";
+  } else {
+    document.getElementById("resposta3").style.display = "none";
+  }
 }
 
 function toggleAnswer4() {
-    var answer4Display = document.getElementById("resposta4").style.display;
+  var answer4Display = document.getElementById("resposta4").style.display;
 
-    if (answer4Display == "none") {
-        document.getElementById("resposta4").style.display = "block";
-    } else {
-        document.getElementById("resposta4").style.display = "none";
-    }
+  if (answer4Display == "none") {
+    document.getElementById("resposta4").style.display = "block";
+  } else {
+    document.getElementById("resposta4").style.display = "none";
+  }
 }
 
 function toggleAnswer5() {
-    var answer5Display = document.getElementById("resposta5").style.display;
+  var answer5Display = document.getElementById("resposta5").style.display;
 
-    if (answer5Display == "none") {
-        document.getElementById("resposta5").style.display = "block";
-    } else {
-        document.getElementById("resposta5").style.display = "none";
-    }
+  if (answer5Display == "none") {
+    document.getElementById("resposta5").style.display = "block";
+  } else {
+    document.getElementById("resposta5").style.display = "none";
+  }
 }
 
 function toggleAnswer6() {
-    var answer6Display = document.getElementById("resposta6").style.display;
+  var answer6Display = document.getElementById("resposta6").style.display;
 
-    if (answer6Display == "none") {
-        document.getElementById("resposta6").style.display = "block";
-    } else {
-        document.getElementById("resposta6").style.display = "none";
-    }
+  if (answer6Display == "none") {
+    document.getElementById("resposta6").style.display = "block";
+  } else {
+    document.getElementById("resposta6").style.display = "none";
+  }
 }
 
 function toggleAnswer7() {
-    var answer7Display = document.getElementById("resposta7").style.display;
+  var answer7Display = document.getElementById("resposta7").style.display;
 
-    if (answer7Display == "none") {
-        document.getElementById("resposta7").style.display = "block";
-    } else {
-        document.getElementById("resposta7").style.display = "none";
-    }
+  if (answer7Display == "none") {
+    document.getElementById("resposta7").style.display = "block";
+  } else {
+    document.getElementById("resposta7").style.display = "none";
+  }
 }
